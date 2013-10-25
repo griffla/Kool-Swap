0521654f2683fbe944c276fb7fa1ce7a42ec95b9
jschmidt <joscha.schmidt@gmail.com>
2013-10-25 11:35:49 +0200

    layout

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="author" content="Joscha Schmidt">
     <meta name="description" content="Rotate all kinds of elements clock- or counterclockwise with jQuery Aperture">
 
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/css/kool-swap.css">
     <link rel="stylesheet" type="text/css" href="/demo/css/demo.css">
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
@@ -14,11 +15,15 @@
 </head>
 
 <body>
-    <div id="main">
-        <a id="dl" href="https://github.com/griffla/Kool-Swap">Watch at GitHub</a>
-        
-        <h1>Hi at Kool Swap</h1>
-        <h2><a href="demo">Watch the demo</a></h2>
+	<div class="container">
+        <div class="main">
+		    <div id="main" class="main-inner col-md-12">
+                <a id="dl" href="https://github.com/griffla/Kool-Swap">Watch at GitHub</a>
+                
+                <h1>Hi at Kool Swap</h1>
+                <p><a href="demo">Watch the demo</a></p>
+            </div>
+        </div>
     </div>
 
     <script src="/demo/js/googleanalytics.js"></script>
